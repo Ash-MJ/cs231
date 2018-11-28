43386f47911c90cafcabbdc29103622a64073ee9
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2018-11-28 05:27:05 -0800

    Update 19.stackLL.c

--- 19.stackLL.c
+++ 19.stackLL.c
@@ -66,3 +66,41 @@ void main()
 	}while(c==1 || c==2 || c==3);
     
 }
+
+
+
+
+OUTPUT:
+
+CHOOSE
+1.PUSH
+2.POP
+3.DISPLAY
+1
+enter the element: 2
+
+CHOOSE
+1.PUSH
+2.POP
+3.DISPLAY
+1
+enter the element: 5
+
+CHOOSE
+1.PUSH
+2.POP
+3.DISPLAY
+3
+elements: 5 ->2 ->NULL
+CHOOSE
+1.PUSH
+2.POP
+3.DISPLAY
+2
+popped out: 5
+CHOOSE
+1.PUSH
+2.POP
+3.DISPLAY
+3
+elements: 2 ->NULL
