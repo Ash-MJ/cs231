0c2f656382f5b20efa35c50236f81179ee8e2a40
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2018-11-28 20:00:59 -0800

    Update 03.insertion sort.c

--- 03.insertion sort.c
+++ 03.insertion sort.c
@@ -1,45 +1,45 @@
-/*
-3.INSERTION SORT
-*/
+	/*
+	3.INSERTION SORT
+	*/
 
-#include<stdio.h>
-void main() 
-{
-	int i,j,n,s,a[50];
-	printf("Enter the size:");
-	scanf("%d",&n);
-	printf("Enter the elements");
-	for(i=0;i<n;i++)
-		{
-			scanf("%d",&a[i]);
-		}
-	for(i=1;i<n;i++)
+	#include<stdio.h>
+	void main() 
 	{
-		s=a[i];
-		j=i-1;
-		while((s<a[j])&&(j>=0))
+		int i,j,n,s,a[50];
+		printf("Enter the size:");
+		scanf("%d",&n);
+		printf("Enter the elements");
+		for(i=0;i<n;i++)
+			{
+				scanf("%d",&a[i]);
+			}
+		for(i=1;i<n;i++)
 		{
-	 		a[j+1]=a[j];
-		   	j=j-1;
+			s=a[i];
+			j=i-1;
+			while((s<a[j])&&(j>=0))
+			{
+				a[j+1]=a[j];
+				j=j-1;
+			}
+			a[j+1]=s;
 		}
-	        a[j+1]=s;
+
+		printf("After Sorting \n");
+		for(i=0; i<n; i++)
+			{
+				printf("%d",a[i]);
+			}
+
 	}
 
-	printf("After Sorting \n");
-	for(i=0; i<n; i++)
-		{
-	  	    	printf("%d",a[i]);
-		}
 
-}
-
-
-OUTPUT:
-Enter the size:4
-Enter the elements1
-3
-2
-7
-After Sorting 
-1237
+	OUTPUT:
+	Enter the size:4
+	Enter the elements1
+	3
+	2
+	7
+	After Sorting 
+	1237
 
