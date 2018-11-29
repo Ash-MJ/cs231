d326484267f7d87cf36b688100e919c080cf4d41
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2018-11-29 09:20:35 +0530

    Update 01.sequential search.c

--- 01.sequential search.c
+++ 01.sequential search.c
@@ -1,46 +1,46 @@
-/*
-1.SEQUENTIAL SEARCH
-*/
+	/*
+	1.SEQUENTIAL SEARCH
+	*/
 
-#include<stdio.h>
-void main()
-{
-	int a[50],k,i,n,p=0;
-	printf("Enter the size");
-	scanf("%d",&n);
-	printf("Enter elements\n");
-	for(i=0;i<n;i++)
+	#include<stdio.h>
+	void main()
 	{
-		scanf("%d",&a[i]);
-	}
-	printf("Enter number to be searched\n");
-	scanf("%d",&k);
-	for(i=0;i<n;i++)
-	{
-		if(a[i]==k)
+		int a[50],k,i,n,p=0;
+		printf("Enter the size");
+		scanf("%d",&n);
+		printf("Enter elements\n");
+		for(i=0;i<n;i++)
 		{
-			p=i+1;
-			break;
+			scanf("%d",&a[i]);
 		}
+		printf("Enter number to be searched\n");
+		scanf("%d",&k);
+		for(i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+				p=i+1;
+				break;
+			}
+		}
+		if(p!=0)
+			printf("%d is found in the list, at position %d\n",k,p);
+		else
+			printf("%d is not in the list\n",k);
 	}
-	if(p!=0)
-		printf("%d is found in the list, at position %d\n",k,p);
-	else
-		printf("%d is not in the list\n",k);
-}
 
 
 
-OUTPUT:
-Enter the size 5
-Enter elements
-3
-1
-8
-2
-5
-Enter number to be searched
-8
-8 is found in the list, at position 3
+	OUTPUT:
+	Enter the size 5
+	Enter elements
+	3
+	1
+	8
+	2
+	5
+	Enter number to be searched
+	8
+	8 is found in the list, at position 3
 
 
