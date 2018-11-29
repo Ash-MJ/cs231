b43960f5d8c63656144c85cc9e11562f4d881cd5
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2018-11-28 20:01:38 -0800

    Update 04.bubblesort.c

--- 04.bubblesort.c
+++ 04.bubblesort.c
@@ -1,49 +1,49 @@
-/*
-4.BUBBLE SORT
-*/
+	/*
+	4.BUBBLE SORT
+	*/
 
-#include<stdio.h>
-void main()
-{
-	int j,i,n,t;	
-	printf("enter number of array elements\n");
-	scanf("%d",&n);
-	int a[n];
-	printf("enter array elements\n");
-	for(i=0;i<n;i++)
+	#include<stdio.h>
+	void main()
 	{
-		scanf("%d",&a[i]);
+		int j,i,n,t;	
+		printf("enter number of array elements\n");
+		scanf("%d",&n);
+		int a[n];
+		printf("enter array elements\n");
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&a[i]);
+		}
+		for(i=0;i<n-1;i++)
+		{
+			for (j=0;j<n-i-1;j++)
+			{
+				if (a[j] > a[j+1])
+					{
+						t=a[j];
+						a[j]=a[j+1];
+						a[j+1] =t;
+					}
+			}
+		}
+		printf("Sorted array:\n");
+		for(i=0;i<n;i++)
+		{
+			printf("%d\n", a[i]);
+		}
 	}
-	for(i=0;i<n-1;i++)
-	{
-		for (j=0;j<n-i-1;j++)
-	  	{
-	    		if (a[j] > a[j+1])
-	    			{
-	      				t=a[j];
-	      				a[j]=a[j+1];
-	      				a[j+1] =t;
-	    			}
-	  	}
-	}
-	printf("Sorted array:\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n", a[i]);
-	}
-}
 
 
-OUTPUT:
-enter number of array elements
-3
-enter array elements
-10
-5
-8
-Sorted array:
-5
-8
-10
+	OUTPUT:
+	enter number of array elements
+	3
+	enter array elements
+	10
+	5
+	8
+	Sorted array:
+	5
+	8
+	10
 
 
