6f6db4b9f13284501249c261d19d1904638df701
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2018-11-28 20:06:51 -0800

    Update 14.circulardoublyLL.c

--- 14.circulardoublyLL.c
+++ 14.circulardoublyLL.c
@@ -1,241 +1,241 @@
-/*
-14.CIRCULAR DOUBLY LL
-*/
+	/*
+	14.CIRCULAR DOUBLY LL
+	*/
 
-#include<stdio.h>
-#include<stdlib.h>
-struct node
-{
-	int data;
-    	struct node *llink,*rlink;
-};
-void main()
-{
-    	int key,x,n,c,l;
-    	struct node *head,*temp,*ptr,*ptr1,*ptr2;
-    	head=(struct node*)malloc(sizeof(struct node));
-    	head->data=0;
-    	head->llink=head;
-    	head->rlink=head;
-	do
-    	{
-        	ptr=head;
-        	printf("\nCHOOSE\n1.Insertion(begining)\n2.Insertion(end)\n3.Insertion(after key)\n4.Deletion\n5.Search\n6.Display\nEnter choice:");
-        	scanf("%d",&c);
-		if(c==1)
-        	{
-            		ptr=head;
-			ptr1=ptr->rlink;
-			temp=(struct node*)malloc(sizeof(struct node));
-			temp->rlink=ptr1;
-			temp->llink=ptr;	
-			ptr->rlink=temp;
-			ptr1->llink=temp;
-            		printf("Enter data: ");
-            		scanf("%d",&x);
-            		temp->data=x;
-        	}
-        	else if(c==2)
-        	{
-            		while(ptr->rlink!=head)
-            		{
-                		ptr=ptr->rlink;
-            		}
-            		temp=(struct node*)malloc(sizeof(struct node));
-            		ptr->rlink=temp;
-			temp->rlink=head;
-            		printf("Enter data: ");
-            		scanf("%d",&x);
-            		temp->data=x;
-            		temp->llink=ptr;
-            		head->llink=temp;
-        	}
-        	else if(c==3)
-        	{
-			ptr=ptr->rlink;
-            		printf("Enter key: ");
-            		scanf("%d",&key);
-            		while(ptr!=head)
-            		{
-                		if(ptr->data==key)
-                		{
-                    			temp=(struct node*)malloc(sizeof(struct node));
-                    			ptr1=ptr->rlink;
-                    			ptr->rlink=temp;
-                    			temp->llink=ptr;
-                    			ptr1->llink=temp;
-                    			temp->rlink=ptr1;
-                    			printf("Enter data: ");
-                    			scanf("%d",&x);
-                    			temp->data=x;
-                		}
-                		ptr=ptr->rlink;
-            		}
-        	}		
-       		else if(c==4)
-        	{
-            		if(ptr->rlink==head)
-            		{
-                		printf("List is empty!!!");
-            		}
-            		else
-            		{
-                		printf("Enter key: ");
-               	 		scanf("%d",&key);
-                		l=0;
-                		while(ptr->rlink!=head)
-                		{
-                    			ptr1=ptr;
-                    			ptr=ptr->rlink;
-                    			ptr2=ptr->rlink;
-                    			if(ptr->data==key)
-                    			{
-                        			if(ptr->rlink==head)
-                        			{
-                            				ptr1=ptr->llink;
-                            				ptr1->rlink=head;
-                            				free(ptr);
-                            				l+=1;    
+	#include<stdio.h>
+	#include<stdlib.h>
+	struct node
+	{
+		int data;
+		struct node *llink,*rlink;
+	};
+	void main()
+	{
+		int key,x,n,c,l;
+		struct node *head,*temp,*ptr,*ptr1,*ptr2;
+		head=(struct node*)malloc(sizeof(struct node));
+		head->data=0;
+		head->llink=head;
+		head->rlink=head;
+		do
+		{
+			ptr=head;
+			printf("\nCHOOSE\n1.Insertion(begining)\n2.Insertion(end)\n3.Insertion(after key)\n4.Deletion\n5.Search\n6.Display\nEnter choice:");
+			scanf("%d",&c);
+			if(c==1)
+			{
+				ptr=head;
+				ptr1=ptr->rlink;
+				temp=(struct node*)malloc(sizeof(struct node));
+				temp->rlink=ptr1;
+				temp->llink=ptr;	
+				ptr->rlink=temp;
+				ptr1->llink=temp;
+				printf("Enter data: ");
+				scanf("%d",&x);
+				temp->data=x;
+			}
+			else if(c==2)
+			{
+				while(ptr->rlink!=head)
+				{
+					ptr=ptr->rlink;
+				}
+				temp=(struct node*)malloc(sizeof(struct node));
+				ptr->rlink=temp;
+				temp->rlink=head;
+				printf("Enter data: ");
+				scanf("%d",&x);
+				temp->data=x;
+				temp->llink=ptr;
+				head->llink=temp;
+			}
+			else if(c==3)
+			{
+				ptr=ptr->rlink;
+				printf("Enter key: ");
+				scanf("%d",&key);
+				while(ptr!=head)
+				{
+					if(ptr->data==key)
+					{
+						temp=(struct node*)malloc(sizeof(struct node));
+						ptr1=ptr->rlink;
+						ptr->rlink=temp;
+						temp->llink=ptr;
+						ptr1->llink=temp;
+						temp->rlink=ptr1;
+						printf("Enter data: ");
+						scanf("%d",&x);
+						temp->data=x;
+					}
+					ptr=ptr->rlink;
+				}
+			}		
+			else if(c==4)
+			{
+				if(ptr->rlink==head)
+				{
+					printf("List is empty!!!");
+				}
+				else
+				{
+					printf("Enter key: ");
+					scanf("%d",&key);
+					l=0;
+					while(ptr->rlink!=head)
+					{
+						ptr1=ptr;
+						ptr=ptr->rlink;
+						ptr2=ptr->rlink;
+						if(ptr->data==key)
+						{
+							if(ptr->rlink==head)
+							{
+								ptr1=ptr->llink;
+								ptr1->rlink=head;
+								free(ptr);
+								l+=1;    
+							}
+							else
+							{
+								ptr1=ptr->llink;
+								ptr2=ptr->rlink;
+								ptr1->rlink=ptr2;
+								ptr2->llink=ptr1;
+								free(ptr);
+								l+=1;
+							}
 						}
-                        			else
-                        			{
-                            				ptr1=ptr->llink;
-                            				ptr2=ptr->rlink;
-                            				ptr1->rlink=ptr2;
-                            				ptr2->llink=ptr1;
-                            				free(ptr);
-                            				l+=1;
-                        			}
-                    			}
-                		}
-                		if(l==0)
-                		{
-                    			printf("Key not found!!!");
-                		}
-            		}
-        	}
-        	else if(c==5)
-        	{
-            		if(ptr->rlink==head)
-            		{
-                		printf("Empty list!!!");
-            		}
-            		else
-            		{
-                		ptr=ptr->rlink;
-                		l=0;
-				n=0;
-                		printf("Enter key: ");
-                		scanf("%d",&key);
-                		while(ptr!=head)
-                		{
-					n=n+1;
-                    			if(ptr->data==key)
-                    			{
-                        			printf("\nElement found at %d",n);
-                        			l+=1;
-                    			}
-                    			ptr=ptr->rlink;
-                		}
-                		if(l==0)
-                		{
-                    			printf("\nElement not found...");
-                		}
-            		}
-        	}
-        	else if(c==6)
-        	{
-            		ptr=head->rlink;
-            		while(ptr->rlink!=head->rlink)
-            		{	
-                		printf("%d    ",ptr->data);
-                		ptr=ptr->rlink;
-            		}
-        	}
-    	}while(c==1 || c==2 || c==3 || c==4 || c==5 || c==6);
-}
+					}
+					if(l==0)
+					{
+						printf("Key not found!!!");
+					}
+				}
+			}
+			else if(c==5)
+			{
+				if(ptr->rlink==head)
+				{
+					printf("Empty list!!!");
+				}
+				else
+				{
+					ptr=ptr->rlink;
+					l=0;
+					n=0;
+					printf("Enter key: ");
+					scanf("%d",&key);
+					while(ptr!=head)
+					{
+						n=n+1;
+						if(ptr->data==key)
+						{
+							printf("\nElement found at %d",n);
+							l+=1;
+						}
+						ptr=ptr->rlink;
+					}
+					if(l==0)
+					{
+						printf("\nElement not found...");
+					}
+				}
+			}
+			else if(c==6)
+			{
+				ptr=head->rlink;
+				while(ptr->rlink!=head->rlink)
+				{	
+					printf("%d    ",ptr->data);
+					ptr=ptr->rlink;
+				}
+			}
+		}while(c==1 || c==2 || c==3 || c==4 || c==5 || c==6);
+	}
 
 
 
 
-OUTPUT:
+	OUTPUT:
 
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:1
-Enter data: 1
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:1
+	Enter data: 1
 
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:2
-Enter data: 3
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:2
+	Enter data: 3
 
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:6
-1    3    
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:3
-Enter key: 1
-Enter data: 2
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:6
+	1    3    
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:3
+	Enter key: 1
+	Enter data: 2
 
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:6
-1    2    3    
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:4
-Enter key: 3
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:6
+	1    2    3    
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:4
+	Enter key: 3
 
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:6
-1    2    
-CHOOSE
-1.Insertion(begining)
-2.Insertion(end)
-3.Insertion(after key)
-4.Deletion
-5.Search
-6.Display
-Enter choice:5
-Enter key: 2
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:6
+	1    2    
+	CHOOSE
+	1.Insertion(begining)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion
+	5.Search
+	6.Display
+	Enter choice:5
+	Enter key: 2
 
-Element found at 2
+	Element found at 2
